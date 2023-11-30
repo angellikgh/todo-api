185c6f64eb5e428d2789a4526b75b7a5acbfd880
gerelt <srintelligh@gmail.com>
2023-11-29 20:44:39 -0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+
+    TodoModule,
+  ],
+})
+export class AppModule {}
